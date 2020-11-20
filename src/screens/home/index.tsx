687481c5e1fe066a2a57b219b8ebb0e7900792d5
Vitor Pereira <vitorsaraivapereira1@gmail.com>
2020-11-20 18:00:05 -0300

    Alterações do boneco

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,9 +3,16 @@ import { Text, TextInput } from 'react-native';
 import { data } from '../../../backend';
 import { JogoController } from '../../../backend/controllers/JogoController';
 
+
 import { Container, Imagem, Texto } from './styles.js';
 
 let Forca = require( '../../assets/0.png');
+let Forca1 = require( '../../assets/1.png');
+let Forca2 = require( '../../assets/2.png');
+let Forca3 = require( '../../assets/3.png');
+let Forca4 = require( '../../assets/4.png');
+let Forca5 = require( '../../assets/5.png');
+let Forca6 = require( '../../assets/6.png');
 
 
 export default function Home() {
@@ -19,55 +26,60 @@ export default function Home() {
   
   function contas(prop:string){
     let response = Jogo.Chutar(prop)
-    setPalavra("")
-
-    if(response.pontuacao.erros >= 1){
-      return setAcertos(response.pontuacao.erros);
-    }
     
-  
-    console.log(response)
+
+    console.log(response.pontuacao.erros)
     console.log(Jogo.caracteres)
     console.log(Jogo.camposAntigos)
     console.log(Jogo.campos)
-
+    return setErros(response.pontuacao.erros);
   }
 
-  useEffect(()=>{
+  // useEffect(()=>{
     
-    /**
-     * Condicionais que fazem com que cada vez que o usuario erre o boneco perca parte do corpo.
-     */
+  //   /**
+  //    * Condicionais que fazem com que cada vez que o usuario erre o boneco perca parte do corpo.
+  //    */
 
-      // if(Jogo.errors === 1){
-      //   Forca = require('../../assets/1.png')
-      // }
-      // if(Jogo.errors === 2){
-      //   Forca = require('../../assets/2.png')
-      // }
-      // if(Jogo.errors === 3){
-      //   Forca = require('../../assets/3.png')
-      // }
-      // if(Jogo.errors === 4){
-      //   Forca = require('../../assets/4.png')
-      // }
-      // if(Jogo.errors === 5){
-      //   Forca = require('../../assets/5.png')
-      // }
-      // if(Jogo.errors === 6){
-      //   Forca = require('../../assets/6.png')
-      // }
+  //     // if(Jogo.errors === 1){
+  //     //   Forca = require('../../assets/1.png')
+  //     // }
+  //     // if(Jogo.errors === 2){
+  //     //   Forca = require('../../assets/2.png')
+  //     // }
+  //     // if(Jogo.errors === 3){
+  //     //   Forca = require('../../assets/3.png')
+  //     // }
+  //     // if(Jogo.errors === 4){
+  //     //   Forca = require('../../assets/4.png')
+  //     // }
+  //     // if(Jogo.errors === 5){
+  //     //   Forca = require('../../assets/5.png')
+  //     // }
+  //     // if(Jogo.errors === 6){
+  //     //   Forca = require('../../assets/6.png')
+  //     // }
 
-    console.log(Jogo.acertos)
-    console.log(Jogo.campos)
+  //   console.log(Jogo.acertos)
+  //   console.log(Jogo.campos)
 
-    console.log(acertos)
-  },[])
+  //   console.log(acertos)
+  // },[])
 
-   console.log(Jogo.errors)
+
+
   return (
+    
     <Container>
-      <Imagem src={Forca} />
+      { erros === 1 ? <Imagem src={Forca1} />
+       : erros ===2 ? <Imagem src={Forca2} />
+       : erros ===3 ? <Imagem src={Forca3} />
+       : erros ===4 ? <Imagem src={Forca4} />
+       : erros ===5 ? <Imagem src={Forca5} />
+       : erros ===6 ? <Imagem src={Forca6} />
+       : <Imagem src={Forca} />
+      }
+      
       <Texto>Categoria:{Jogo.categoria}</Texto>
       <Texto>{Jogo.campos}</Texto>
       <TextInput value={palavra} maxLength={1} onChange={(e) => contas(e.target.value) } />  
